10354c62cc3bd0b9de6dc141a55acd939aa63bce
alejandroES10 <aleessu@gmail.com>
2024-09-23 17:48:03 -0400

    CORS

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from api.controllers import documents_controller
 from database.vector_store import collection__of__books
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 @app.get("/")
 async def read_root():
