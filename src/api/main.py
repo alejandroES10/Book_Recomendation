3075ae976533cdafc2778f115fbe64def2dfe635
alejandroES10 <aleessu@gmail.com>
2025-05-14 17:55:33 -0400

    updated

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends
-from src.api.controllers import chat_controller, general_information_controller, material_controller
+from src.api.controllers import book_metadata_controller, chat_controller, general_information_controller
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.security.auth import validate_api_key
 
@@ -20,7 +20,7 @@ async def read_root():
     return "Tesis de Alejandro Estrada Suárez"
 
 
-app.include_router(material_controller.router, prefix="/materials")
+app.include_router(book_metadata_controller.router, prefix="/materials")
 
 app.include_router(chat_controller.router, prefix="/chat")
 app.include_router(general_information_controller.router, prefix="/general_information",dependencies=[Depends(validate_api_key)])
